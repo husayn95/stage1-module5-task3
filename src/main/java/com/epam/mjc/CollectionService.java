375f3f560afab66df340fca0e799c3e06a4ca614
Husayn Ganiev <hg222ii@student.lnu.se>
2023-02-27 08:15:30 +0100

    first try

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,27 +1,53 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
-    public List<Integer> filterEvenNumbers(List<Integer> list) {
+    public static List<Integer> filterEvenNumbers(List<Integer> list) {
+        return list.stream().filter(integer -> integer % 2 == 0).collect(Collectors.toList());
+        //throw new UnsupportedOperationException("You should implement this method.");
+    }
+
+    public static List<String> toUpperCaseCollection(List<String> list) {
+
+        return list.stream().map(s -> s.toUpperCase()).collect(Collectors.toList());
+        //throw new UnsupportedOperationException("You should implement this method.");
+    }
+
+    public static Optional<Integer> findMax(List<Integer> list) {
+        return list.stream().max(Integer::compareTo);
+        //throw new UnsupportedOperationException("You should implement this method.");
+    }
+
+    public  Optional<Integer> findMin(List<List<Integer>> list) {
+       // return list.stream().flatMap(list - > list.stream().min(Integer::compare));
+
         throw new UnsupportedOperationException("You should implement this method.");
     }
 
-    public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+    public static Integer sum(List<Integer> list) {
+       return list.stream().reduce((n,e) -> n + e).get();
+        // throw new UnsupportedOperationException("You should implement this method.");
     }
 
-    public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
-
-    public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
-
-    public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(6);
+        System.out.println(filterEvenNumbers(integers));
+        System.out.println(findMax(integers));
+        System.out.println(sum(integers));
+        List<String> strings = new ArrayList<>();
+        strings.add("asdasd");
+        strings.add("asdsasd");
+        strings.add("asddasd");
+        strings.add("asdasfd");
+        System.out.println(toUpperCaseCollection(strings));
     }
 }
